e5c4b79f87b76bb8738b66e1b77807a1201140d5
Victor Paim <91322495+LabuPaim@users.noreply.github.com>
2022-03-14 01:05:41 -0300

    Teclas_adicionadas

--- Assets/JS/index.js
+++ Assets/JS/index.js
@@ -76,6 +76,48 @@ elementoBTN_right.addEventListener('click', () => {
   }
 });
 
+document.addEventListener('keydown', event => {
+  const tecla = event.keyCode;
+  let posicao = elementoBTN_left.value - 1;
+  if (tecla == 37) {
+    if (posicao > -1) {
+      elementoNOME.innerText = champs[posicao].nome;
+      elementoROTA.innerText = champs[posicao].rota;
+      elementoIMAGEM.src = champs[posicao].imagem;
+      elementoVIDEO.src = champs[posicao].video;
+      elementoBTN_left.value = posicao;
+    } else {
+      posicao = champs.length - 1;
+      elementoNOME.innerText = champs[posicao].nome;
+      elementoROTA.innerText = champs[posicao].rota;
+      elementoIMAGEM.src = champs[posicao].imagem;
+      elementoVIDEO.src = champs[posicao].video;
+      elementoBTN_left.value = posicao;
+    }
+  }
+});
+
+document.addEventListener('keydown', event => {
+  const tecla = event.keyCode;
+  let posicao = parseInt(elementoBTN_left.value) + 1;
+  if (tecla == 39) {
+    if (posicao >= champs.length) {
+      posicao = 0;
+      elementoNOME.innerText = champs[posicao].nome;
+      elementoROTA.innerText = champs[posicao].rota;
+      elementoIMAGEM.src = champs[posicao].imagem;
+      elementoVIDEO.src = champs[posicao].video;
+      elementoBTN_left.value = posicao;
+    } else {
+      elementoNOME.innerText = champs[posicao].nome;
+      elementoROTA.innerText = champs[posicao].rota;
+      elementoIMAGEM.src = champs[posicao].imagem;
+      elementoVIDEO.src = champs[posicao].video;
+      elementoBTN_left.value = posicao;
+    }
+  }
+});
+
 elementoKEY_Q.addEventListener('click', () => {
   for (ch of champs) {
     if (elementoNOME.innerText == ch.nome) {
@@ -108,89 +150,20 @@ elementoKEY_R.addEventListener('click', () => {
   }
 });
 
-elementoShare_id.addEventListener('click', () => {
-  if (elementoShare_id.value == '0') {
-    elementoOne_id.animate(
-      {
-        top: '160px',
-        left: '50%',
-      },
-      200,
-    );
 
-    $(this).siblings('.one').animate(
-      {
-        top: '160px',
-        left: '50%',
-      },
-      200,
-    );
-
-    $(this).siblings('.two').delay(200).animate(
-      {
-        top: '260px',
-        left: '40%',
-      },
-      200,
-    );
-
-    $(this).siblings('.three').delay(300).animate(
-      {
-        top: '260px',
-        left: '60%',
-      },
-      200,
-    );
-
-    $('.one i,.two i, .three i').delay(500).fadeIn(200);
-    elementoShare_id.value = '1';
-  } else {
-    $('.one, .two, .three').animate(
-      {
-        top: '300px',
-        left: '50%',
-      },
-      200,
-    );
-
-    $('.one i,.two i, .three i').delay(500).fadeOut(200);
-    elementoShare_id.value = '0';
+document.addEventListener('keydown', event => {
+  const tecla = event.keyCode;
+  for (ch of champs) {
+    if (elementoNOME.innerText == ch.nome) {
+      if (tecla == 81) {
+        elementoVIDEO.src = 'Assets/Champs/' + ch.nome + '/Q1.webm';
+      } else if (tecla == 87) {
+        elementoVIDEO.src = 'Assets/Champs/' + ch.nome + '/W1.webm';
+      } else if (tecla == 82) {
+        elementoVIDEO.src = 'Assets/Champs/' + ch.nome + '/R1.webm';
+      } else if (tecla == 69) {
+        elementoVIDEO.src = 'Assets/Champs/' + ch.nome + '/E1.webm';
+      }
+    }
   }
 });
-
-// $(function(){
-//   var flag=0;
-
-//   $('.share').on('click',function(){
-//    if(flag == 0)
-//     {
-//       $(this).siblings('.one').animate({
-//       top:'160px',
-//       left:'50%',
-//     },200);
-
-//      $(this).siblings('.two').delay(200).animate({
-//       top:'260px',
-//       left:'40%'
-//     },200);
-
-//      $(this).siblings('.three').delay(300).animate({
-//       top:'260px',
-//       left:'60%'
-//     },200);
-
-//     $('.one i,.two i, .three i').delay(500).fadeIn(200);
-//       flag = 1;
-//     }
-
-//   else{
-//     $('.one, .two, .three').animate({
-//         top:'300px',
-//         left:'50%'
-//       },200);
-
-//   $('.one i,.two i, .three i').delay(500).fadeOut(200);
-//       flag = 0;
-//     }
-//   });
-// });
